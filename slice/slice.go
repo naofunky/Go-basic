b75c5d8718bd5cff7d308c215c799f4e4b720cc4
Koki Nanao <minsokosoko0107@gmail.com>
2023-09-05 19:09:16 +0900

    更新

---
+++ slice/slice.go
@@ -0,0 +1,19 @@
+package slice
+
+import "fmt"
+
+func slice() {
+	var a [3]int
+	var a1 = [3]int{1, 2, 3}
+	a2 := [...]int{1, 2, 3, 4, 5}
+
+	var s1 []int
+	s2 := []int{}
+
+	fmt.Println(a)
+	fmt.Println(a1)
+	fmt.Println(a2)
+	fmt.Println(len(a2), cap(a2))
+	fmt.Println(s1)
+	fmt.Println(s2)
+}
