e3f45165a402b9116c37df111099904f8214aa14
Koki Nanao <minsokosoko0107@gmail.com>
2023-10-12 22:37:20 +0900

    ポインタの使い所

--- main.go
+++ main.go
@@ -1,20 +1,42 @@
 package main
 
-import (
-	"fmt"
-	// "os"
-	// "github.com/joho/godotenv"
-)
+import "fmt"
+
+// "os"
+// "github.com/joho/godotenv"
 
 var client *string
 
 func main() {
-	test := "test"
-	var pointer *string = &test
+	// test := "test"
+	// var pointer *string = &test
 
-	client = &test
+	// client = &test
 
-	defer fmt.Println(test)
-	fmt.Println(pointer)
-	fmt.Println(*client)
+	// defer fmt.Println(test)
+	// fmt.Println(pointer)
+	// fmt.Println(*client)
+	forname := User{name: "test"}
+	getName(forname)
+	fmt.Println(forname.name)
+
+	// forname := &User{name: "test"}
+	// getName(forname)
+	// fmt.Println(forname.name)
+
+	s := []string{"a", "b", "c"}
+	changeSlice(s)
+	fmt.Println(s)
+}
+
+type User struct {
+	name string
+}
+
+func getName(u User) {
+	u.name = "test2"
+}
+
+func changeSlice(s []string) {
+	s[0] = "d"
 }
