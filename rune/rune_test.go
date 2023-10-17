c82de8857adb0626776f3645a91bc782b9768276
Koki Nanao <minsokosoko0107@gmail.com>
2023-10-17 16:10:07 +0900

    feat:追加
    runeのコードの検証

--- rune/rune_test.go
+++ rune/rune_test.go
@@ -1,1 +1,21 @@
 package main
+
+import "testing"
+
+func TestRune(t *testing.T) {
+
+	t.Run("半角英数字の文字数カウント検証", func(t *testing.T) {
+		message := "A"
+		if len(message) != 1 {
+			t.Errorf("length is not 1")
+		}
+	})
+
+	t.Run("日本語の文字数カウント検証", func(t *testing.T) {
+		message := "こんにちは"
+		messageRune := []rune(message)
+		if len(messageRune) != 5 {
+			t.Errorf("character length is not 5")
+		}
+	})
+}
