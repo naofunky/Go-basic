9804cabeebc8d062b82295ef812cc977f5ef8349
Koki Nanao <minsokosoko0107@gmail.com>
2023-10-18 15:45:44 +0900

    feat:追加
    time.Timeの比較演算子の実践だが、うまくいかない

--- time/time.go
+++ time/time.go
@@ -5,14 +5,31 @@ import (
 	"time"
 )
 
+type MyTime struct {
+	t time.Time
+}
+
 func main() {
-	// ローカル時間を取得
-	localTime := time.Now()
+	// // ローカル時間を取得
+	// localTime := time.Now()
 
-	// UTCに変換
-	utcTime := localTime.UTC()
+	// // UTCに変換
+	// utcTime := localTime.UTC()
 
-	// ローカル時間とUTC時間を出力
-	fmt.Printf("Local time: %s\n", localTime)
-	fmt.Printf("UTC time: %s\n", utcTime)
+	// // ローカル時間とUTC時間を出力
+	// fmt.Printf("Local time: %s\n", localTime)
+	// fmt.Printf("UTC time: %s\n", utcTime)
+
+	t1 := MyTime{t: time.Now()}
+	t2 := MyTime{t: time.Now()}
+
+	// invalid operation: t1 == t2 (struct containing time.Time cannot be compared)というコンパイルエラーになるのかなと思ったら処理が実行されてしまう。
+	if t1.t.Equal(t2.t) {
+		fmt.Println("t1 is equal to t2")
+	} else {
+		fmt.Println("t1 is not equal to t2")
+	}
+
+	// fmt.Printf("Local time: %s\n", t1)
+	// fmt.Printf("Local time: %s\n", t2)
 }
