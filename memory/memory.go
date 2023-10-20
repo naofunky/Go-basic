291c1d930e034cf1b5287ddd6a9233cbaed3d176
Koki Nanao <minsokosoko0107@gmail.com>
2023-10-20 14:08:55 +0900

    feat:追加
    ヒープの状態、メモリに当てられた量、GCの回数を出力

---
+++ memory/memory.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"runtime"
+)
+
+func printMemStats() {
+	var m runtime.MemStats
+	runtime.ReadMemStats(&m)
+	// 現在のヒープの状態を出力
+	fmt.Printf("Alloc = %v MiB", bToMb(m.Alloc))
+	// ヒープに割り当てられた累計量
+	fmt.Printf("\tTotalAlloc = %v MiB", bToMb(m.TotalAlloc))
+	// システムがGoランタイムに割り当てたメモリ量
+	fmt.Printf("\tSys = %v MiB", bToMb(m.Sys))
+	// そしてガベージコレクションの回数
+	fmt.Printf("\tNumGC = %v\n", m.NumGC)
+}
+
+func bToMb(b uint64) uint64 {
+	return b / 1024 / 1024
+}
+
+func main() {
+	printMemStats()
+
+	// 思い処理なので注意
+	data := make([]int, 1<<25) // 2^25要素のスライスを作成
+	for i := range data {
+		data[i] = i
+	}
+	printMemStats()
+}
