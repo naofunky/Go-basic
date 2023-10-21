e984e38ae457e6e5c3ab51d3f7996d5a1d4b56b7
Koki Nanao <minsokosoko0107@gmail.com>
2023-10-21 17:25:48 +0900

    feat:追加
    スライスでmakeメソッドを用いた初期化

--- slice/slice.go
+++ slice/slice.go
@@ -13,19 +13,23 @@ func main() {
 	// 	fmt.Println(v)
 	// }
 
-	var nilSlice []int
-	emptySlice := []int{}
+	// var nilSlice []int
+	// emptySlice := []int{}
 
-	fmt.Println(nilSlice == nil)
-	fmt.Println(emptySlice == nil)
+	// fmt.Println(nilSlice == nil)
+	// fmt.Println(emptySlice == nil)
 
-	fmt.Println(len(nilSlice))
-	fmt.Println(len(emptySlice))
+	// fmt.Println(len(nilSlice))
+	// fmt.Println(len(emptySlice))
 
-	fmt.Println(cap(nilSlice))
-	fmt.Println(cap(emptySlice))
+	// fmt.Println(cap(nilSlice))
+	// fmt.Println(cap(emptySlice))
 
 	// fmt.Println(sl)
 	// fmt.Println(sl[0])
 	// fmt.Println(len(sl))
+
+	s4 := make([]int, 0, 3)
+	s4 = append(s4, 1, 2, 3, 4, 5, 6, 7, 8, 9)
+	fmt.Println(s4, len(s4), cap(s4))
 }
