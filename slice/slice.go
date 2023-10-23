1910eeb0670620dead96aab3e986ec2325b7a342
Koki Nanao <minsokosoko0107@gmail.com>
2023-10-23 16:58:53 +0900

    fix:修正
    スライスの身完全理解ver

--- slice/slice.go
+++ slice/slice.go
@@ -29,9 +29,12 @@ func main() {
 	// fmt.Println(sl[0])
 	// fmt.Println(len(sl))
 
-	// s4 := make([]int, 0, 3)
-	// s4 = append(s4, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15)
-	// fmt.Println(s4, len(s4), cap(s4))
+	s4 := make([]int, 0, 4)
+	fmt.Println(s4, len(s4), cap(s4))
+	s5 := append(s4, 1, 2, 3, 4, 5, 6, 7, 8, 9)
+	fmt.Println(s5, len(s5), cap(s5))
+	// s76 := append(s4, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15)
+	// fmt.Println(s76, len(s76), cap(s76))
 
 	// s5 := make([]int, 3, 6)
 	// fmt.Println(s5, len(s5), cap(s5))
@@ -51,8 +54,13 @@ func main() {
 	// fmt.Println(s9, len(s9), cap(s9))
 	// fmt.Println(s10, len(s10), cap(s10))
 
-	s11 := []int{11, 12, 13, 14, 15, 16, 17, 18} // 8,8
-	s12 := s11[2:6]                              // 4,6
-	fmt.Println(s11, len(s11), cap(s11))
-	fmt.Println(s12, len(s12), cap(s12))
+	// s11 := []int{11, 12, 13, 14, 15, 16, 17, 18} // 8,8
+	// s12 := s11[2:6]                              // 4,6
+	// fmt.Println(s11, len(s11), cap(s11))
+	// fmt.Println(s12, len(s12), cap(s12))
+
+	// s13 := []int{35, 69, 101, 2, 52, 223, 4035, 22, 76, 99} // 10,10
+	// s14 := s13[3:7]
+	// fmt.Println(s13, len(s13), cap(s13))
+	// fmt.Println(s14, len(s14), cap(s14))
 }
