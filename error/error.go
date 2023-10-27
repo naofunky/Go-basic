37ee0dc4e2855e69b2ce659e5124654d753e33b7
Koki Nanao <minsokosoko0107@gmail.com>
2023-10-27 15:41:00 +0900

    feat:追加
    エラーの追加

--- error/error.go
+++ error/error.go
@@ -3,9 +3,30 @@ package main
 import (
 	"errors"
 	"fmt"
+	"os"
 )
 
 func main() {
 	err01 := errors.New("something wrong")
 	fmt.Printf("%[1]p %[1]T %[1]v\n", err01)
+
+	file := "dummy.txt"
+	err3 := fileChecker(file)
+
+	if err3 != nil {
+		if errors.Is(err3, os.ErrNotExist) {
+			fmt.Printf("%v file not found\n", file)
+		} else {
+			fmt.Println("unknown error")
+		}
+	}
+}
+
+func fileChecker(name string) error {
+	f, err := os.Open(name)
+	if err != nil {
+		return fmt.Errorf("in checker: %w", err)
+	}
+	defer f.Close()
+	return nil
 }
