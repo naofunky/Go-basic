6ee2f707ba28b66f874179954c43bcb6425119ba
Koki Nanao <minsokosoko0107@gmail.com>
2023-10-28 21:15:30 +0900

    feat:追加
    内容追記

--- generics/generics.go
+++ generics/generics.go
@@ -1,1 +1,14 @@
 package main
+
+import "fmt"
+
+type customConstraints interface {
+	int | int16 | float32 | float64 | string
+}
+
+func add[T customConstraints](x, y T) T {
+	return x + y
+}
+func main() {
+	fmt.Printf("%v\n", add(1, 2))
+}
