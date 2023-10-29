018cca917dcc294ca03e4c8abe47387c987e9afd
Koki Nanao <minsokosoko0107@gmail.com>
2023-10-29 12:52:18 +0900

    feat:追加
    テスト内容追記で検証

--- generics/generics_test.go
+++ generics/generics_test.go
@@ -2,5 +2,31 @@ package main
 
 import "testing"
 
+// generics/generics.goのコードをテストする
 func TestGenerics(t *testing.T) {
+	t.Run("int型の加算", func(t *testing.T) {
+		if add(1, 2) != 3 {
+			t.Errorf("add(1, 2) is not 3")
+		}
+	})
+	t.Run("int16型の加算", func(t *testing.T) {
+		if add(int16(1), int16(2)) != int16(3) {
+			t.Errorf("add(int16(1), int16(2)) is not int16(3)")
+		}
+	})
+	t.Run("float32型の加算", func(t *testing.T) {
+		if add(float32(1.0), float32(2.0)) != float32(3.0) {
+			t.Errorf("add(float32(1.0), float32(2.0)) is not float32(3.0)")
+		}
+	})
+	t.Run("float64型の加算", func(t *testing.T) {
+		if add(float64(1.0), float64(2.0)) != float64(3.0) {
+			t.Errorf("add(float64(1.0), float64(2.0)) is not float64(3.0)")
+		}
+	})
+	t.Run("string型の加算", func(t *testing.T) {
+		if add("1", "2") != "12" {
+			t.Errorf("add(\"1\", \"2\") is not \"12\"")
+		}
+	})
 }
