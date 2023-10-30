9cf91ed3abd7569c224c06a82712108a2ea8228f
Koki Nanao <minsokosoko0107@gmail.com>
2023-10-30 15:04:20 +0900

    feat:追加
    interfaceを元に型を定義を作成

--- generics/generics.go
+++ generics/generics.go
@@ -1,14 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
+// コンストレンズ
 type customConstraints interface {
-	int | int16 | float32 | float64 | string
+	~int | int16 | float32 | float64 | string
 }
 
+type NewInt int
+
+// 型パラメータを持つ関数
 func add[T customConstraints](x, y T) T {
 	return x + y
 }
 func main() {
 	fmt.Printf("%v\n", add(1, 2))
+
+	var i1, i2 NewInt = 3, 4
+	fmt.Printf("%v\n", add(i1, i2))
 }
