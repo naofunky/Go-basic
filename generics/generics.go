5c89431d1b677f42762cf3782bfda92b7f15ae8e
Koki Nanao <minsokosoko0107@gmail.com>
2023-10-30 15:10:24 +0900

    feat:追加
    int16もコンストレンズを元に作成された型を定義

--- generics/generics.go
+++ generics/generics.go
@@ -6,10 +6,11 @@ import (
 
 // コンストレンズ
 type customConstraints interface {
-	~int | int16 | float32 | float64 | string
+	~int | ~int16 | float32 | float64 | string
 }
 
 type NewInt int
+type NewInt16 int16
 
 // 型パラメータを持つ関数
 func add[T customConstraints](x, y T) T {
@@ -19,5 +20,7 @@ func main() {
 	fmt.Printf("%v\n", add(1, 2))
 
 	var i1, i2 NewInt = 3, 4
+	var i3, i4 NewInt16 = 16, 23
 	fmt.Printf("%v\n", add(i1, i2))
+	fmt.Printf("%v\n", add(i3, i4))
 }
