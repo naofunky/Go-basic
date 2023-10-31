e71bdea547bdf74e988a3f461a1ae1b6c06c18ab
Koki Nanao <minsokosoko0107@gmail.com>
2023-10-31 22:44:20 +0900

    feat:追記
    goroutineの内容を追記

--- goroutine/goroutine.go
+++ goroutine/goroutine.go
@@ -1,5 +1,15 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"runtime"
+)
 
+func main() {
+	// 関数をゴルーチンとして実行
+	go func() {
+		fmt.Println("goroutine invoked")
+	}()
+	fmt.Printf("num of working goroutines: %d\n", runtime.NumGoroutine())
+	fmt.Println("main func finish")
 }
