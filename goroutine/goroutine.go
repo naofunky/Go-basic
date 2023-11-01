e45bd7c533ecff1e5e1c04e85dd1f25136b61602
Koki Nanao <minsokosoko0107@gmail.com>
2023-11-01 16:13:06 +0900

    feat:追加
    ゴルーチンの完了処理の追記

--- goroutine/goroutine.go
+++ goroutine/goroutine.go
@@ -3,13 +3,18 @@ package main
 import (
 	"fmt"
 	"runtime"
+	"sync"
 )
 
 func main() {
+	var wg sync.WaitGroup
+	wg.Add(1)
 	// 関数をゴルーチンとして実行
 	go func() {
+		defer wg.Done()
 		fmt.Println("goroutine invoked")
 	}()
+	wg.Wait()
 	fmt.Printf("num of working goroutines: %d\n", runtime.NumGoroutine())
 	fmt.Println("main func finish")
 }
