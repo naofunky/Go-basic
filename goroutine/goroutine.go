21306788295805aab67ae2ab3c9ddbf7257a7d0d
Koki Nanao <minsokosoko0107@gmail.com>
2023-11-02 17:56:44 +0900

    feat:追加
    tracerの処理をゴルーチンで並行処理にする

--- goroutine/goroutine.go
+++ goroutine/goroutine.go
@@ -1,8 +1,14 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"log"
 	"os"
+	"runtime"
+	"runtime/trace"
+	"sync"
+	"time"
 )
 
 func main() {
@@ -23,9 +29,48 @@ func main() {
 		log.Fatalln("Error:", err)
 	}
 
+	// 他の処理の後にファイルを閉じる関数をdeferで遅延発火
 	defer func() {
 		if err := f.Close(); err != nil {
 			log.Fatalln("Error:", err)
 		}
 	}()
+
+	// tracerをtrace.outファイルを開くのにバリデーション
+	if err := trace.Start(f); err != nil {
+		log.Fatalln("Error:", err)
+	}
+
+	defer trace.Stop()
+	ctx, t := trace.NewTask(context.Background(), "main")
+	defer t.End()
+	fmt.Println("The number of logical CPU Cores:", runtime.NumCPU())
+
+	// 逐次処理
+	// task(ctx, "task1")
+	// task(ctx, "task2")
+	// task(ctx, "task3")
+
+	// ゴルーチン処理
+	var wg sync.WaitGroup
+	wg.Add(3)
+	go cTack(ctx, &wg, "Task1")
+	go cTack(ctx, &wg, "Task2")
+	go cTack(ctx, &wg, "Task3")
+	wg.Wait()
+}
+
+// 逐次関数処理
+// func task(ctx context.Context, name string) {
+// 	defer trace.StartRegion(ctx, name).End()
+// 	time.Sleep(time.Second)
+// 	fmt.Println(name)
+// }
+
+// goroutineを利用してタスクを並行処理
+func cTack(ctx context.Context, wg *sync.WaitGroup, name string) {
+	defer trace.StartRegion(ctx, name).End()
+	defer wg.Done()
+	time.Sleep(time.Second)
+	fmt.Println(name)
 }
