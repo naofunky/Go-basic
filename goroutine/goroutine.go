b1ad4940f502cc75cde7da92975834ca1576ec71
Koki Nanao <minsokosoko0107@gmail.com>
2023-11-02 16:01:29 +0900

    feat:追記
    tracer機能の追記

--- goroutine/goroutine.go
+++ goroutine/goroutine.go
@@ -1,20 +1,31 @@
 package main
 
 import (
-	"fmt"
-	"runtime"
-	"sync"
+	"log"
+	"os"
 )
 
 func main() {
-	var wg sync.WaitGroup
-	wg.Add(1)
-	// 関数をゴルーチンとして実行
-	go func() {
-		defer wg.Done()
-		fmt.Println("goroutine invoked")
+	// var wg sync.WaitGroup
+	// wg.Add(1)
+	// // 関数をゴルーチンとして実行
+	// go func() {
+	// 	defer wg.Done()
+	// 	fmt.Println("goroutine invoked")
+	// }()
+	// wg.Wait()
+	// fmt.Printf("num of working goroutines: %d\n", runtime.NumGoroutine())
+	// fmt.Println("main func finish")
+
+	// トレーサーの作成
+	f, err := os.Create("trace.out")
+	if err != nil {
+		log.Fatalln("Error:", err)
+	}
+
+	defer func() {
+		if err := f.Close(); err != nil {
+			log.Fatalln("Error:", err)
+		}
 	}()
-	wg.Wait()
-	fmt.Printf("num of working goroutines: %d\n", runtime.NumGoroutine())
-	fmt.Println("main func finish")
 }
