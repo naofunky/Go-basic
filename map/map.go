c85b34cf4364b2263e854b52ac115cebc9e15543
Koki Nanao <minsokosoko0107@gmail.com>
2024-06-27 00:17:17 +0900

    feat

--- map/map.go
+++ map/map.go
@@ -4,10 +4,10 @@ import "fmt"
 
 func main() {
 	// 宣言法
-	m := map[string]string{}
-	var nilmap map[string]int
-	fmt.Println(len(nilmap))
-	fmt.Println(m)
+	// m := map[string]string{}
+	// var nilmap map[string]int
+	// fmt.Println(len(nilmap))
+	// fmt.Println(m)
 
 	// 初期化
 	mo := map[string]string{
@@ -22,17 +22,18 @@ func main() {
 	mo["sex"] = "woman"
 
 	// 値の取得はキーを指定する
-	fmt.Println(mo["name"])
+	// fmt.Println(mo["name"])
 
 	// 要素を取得する際に第二戻り値を用意すると、keyの有無を調べることができる
-	// v, ok := m["sex"]
-	// fmt.Println(v)
-	// fmt.Println(ok)
+	v, ok := mo["sex"]
+	fmt.Println("keyに対しての値確認:", v)
+	fmt.Println("keyの存在確認:", ok)
 
-	// for k, v := range m {
-	// 	fmt.Println(k)
-	// 	fmt.Println(v)
-	// }
+	for k, v := range mo {
+		fmt.Println(
+			"key:", k, "value:", v,
+		)
+	}
 
 	// mapの削除機能は第一引数には削除対象のmap第二引数には削除する要素のキーを指定する
 	// delete(m, "name")
