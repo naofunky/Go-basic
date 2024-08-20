90fd29003067edebf8f444c645b1a469253c2b83
Koki Nanao <minsokosoko0107@gmail.com>
2024-08-21 01:03:23 +0900

    feat

--- defer/defer.go
+++ defer/defer.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"log"
 	"os"
 )
@@ -19,6 +20,18 @@ func main() {
 	defer f.Close()
 
 	// fmt.Println("ファイルを指定できました")
-	fmt.Println(f)
-	log.SetFlags(log.Lshortfile)
+	// fmt.Println(f)
+
+	data := make([]byte, 2048)
+	fmt.Println((data))
+	for {
+		count, err := f.Read(data)
+		os.Stdout.Write(data[:count])
+		if err != nil {
+			if err != io.EOF {
+				log.Fatal(err)
+			}
+			break
+		}
+	}
 }
